ddc9e7bf7a2bb6d7b8f3d2c0f3b0d2cd80f82a9f
Dwayne Schultz <myshkin5@users.noreply.github.com>
2022-10-19 15:36:56 -0600

    Better default options to drip script

--- scripts/traffic/constant-traffic.sh
+++ scripts/traffic/constant-traffic.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+set -euEo pipefail
+
+OPTS=("-n" "10000" "-c" "20")
+if [ $# -gt 0 ]; then
+  OPTS=("$@")
+fi
+
 while true; do
   POD=$(kubectl get pods -n traffic-client -l app=client -o jsonpath='{.items[0].metadata.name}' 2> /dev/null)
 
@@ -8,9 +15,10 @@ while true; do
   if [[ $? != 0 ]]; then
     kubectl cp ~/workspace/hey_linux_amd64 -c client "traffic-client/$POD:hey" > /dev/null 2>&1
     kubectl exec -n traffic-client -c client "$POD" -- chmod +x /hey > /dev/null 2>&1
+    sleep 5
     SPACE="^"
   fi
 
   echo "$(date)$SPACE$( (kubectl exec -n traffic-client "$POD" -c client -- \
-      ./hey -n 10000 -c 1000 http://server.traffic-server.svc.cluster.local:8000/get | grep responses) 2> /dev/null )"
+      ./hey "${OPTS[@]}" http://server.traffic-server.svc.cluster.local:8000/get | grep responses) 2> /dev/null )"
 done
